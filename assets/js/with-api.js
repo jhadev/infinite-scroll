43ae2f36b225f4b01fd83e2bcef54cdfbb04a3d9
jhadev <joshappeldev@gmail.com>
2020-07-14 09:48:08 -0400

    media queries/formatting

--- assets/js/with-api.js
+++ assets/js/with-api.js
@@ -1,5 +1,5 @@
-const scroller = document.querySelector("#scroller");
-const sentinel = document.querySelector("#sentinel");
+const scroller = document.querySelector('#scroller');
+const sentinel = document.querySelector('#sentinel');
 
 // counter to stop fetching
 let totalItems = 0;
@@ -38,30 +38,30 @@ async function loadItems() {
 
   // stop loading at 100 items
   if (totalItems > 100) {
-    const newItem = document.createElement("div");
-    newItem.classList.add("item");
+    const newItem = document.createElement('div');
+    newItem.classList.add('item');
     newItem.textContent = `no more items`;
     scroller.appendChild(newItem);
     // stop IntersectionObserver
     observer.unobserve(sentinel);
-    return console.log("no more items");
+    return console.log('no more items');
   }
 
   apiData.forEach((item) => {
-    const newItem = document.createElement("div");
-    newItem.classList.add("item");
-    const ul = document.createElement("ul");
+    const newItem = document.createElement('div');
+    newItem.classList.add('item');
+    const ul = document.createElement('ul');
 
-    if ("picture" in item) {
-      let img = document.createElement("img");
-      img.classList.add("photo");
-      img.setAttribute("data-src", item.picture);
+    if ('picture' in item) {
+      let img = document.createElement('img');
+      img.classList.add('photo');
+      img.setAttribute('data-src', item.picture);
       newItem.appendChild(img);
     }
 
     for (let [key, value] of Object.entries(item)) {
-      if (key !== "picture") {
-        const li = document.createElement("li");
+      if (key !== 'picture') {
+        const li = document.createElement('li');
         li.innerHTML = `${key}: ${value}`;
         ul.appendChild(li);
       }
@@ -84,7 +84,7 @@ async function callback(entries) {
 
 // swap data-src attr with src function
 function loadImage(img) {
-  const src = img.getAttribute("data-src");
+  const src = img.getAttribute('data-src');
   if (!src) {
     return;
   }
@@ -94,13 +94,13 @@ function loadImage(img) {
 // function that returns IntersectionObserver instance for lazy loading
 function imgObserver() {
   const options = {
-    rootMargin: "0px 0px 20px 0px",
+    rootMargin: '0px 0px 20px 0px',
     threshold: 0.5,
   };
 
   const observer = new IntersectionObserver((entries, self) => {
     entries.forEach((entry) => {
-      console.log("entry: ", entry);
+      console.log('entry: ', entry);
       if (entry.isIntersecting) {
         loadImage(entry.target);
         // stop watching element after img is loaded
@@ -116,7 +116,7 @@ function lazyLoad() {
   // observer instance
   const lazyLoader = imgObserver();
   // select all elements with data-src attr
-  const photos = document.querySelectorAll("[data-src]");
+  const photos = document.querySelectorAll('[data-src]');
 
   // loop over each img and observe for lazy loading
   photos.forEach((photo) => {
@@ -127,7 +127,7 @@ function lazyLoad() {
 // new instance of InterSectionObserver for infinite scrolling/auto fetching
 const fetchObserver = new IntersectionObserver(callback, {
   threshold: 1,
-  rootMargin: "0% 0% 5% 0%",
+  rootMargin: '0% 0% 5% 0%',
 });
 
 // load initial items and observe the sentinel id to load more once user scrolls to that element
